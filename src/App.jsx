a92132b46f4fcd4a374a79cd2da839d40faf4984
Suyash <mishrasuyash2018@gmail.com>
2025-08-29 16:03:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="h-screen p-10">
+        <div className="border-2 border-white rounded-xl w-40 p-5 flex items-center justify-center text-xl">
+          <div className="text-white">BBG</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
