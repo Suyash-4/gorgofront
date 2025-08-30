69064a0cf603f9a2a72e41ef7e64432809879590
Suyash <mishrasuyash2018@gmail.com>
2025-08-30 15:57:51 +0530

    Agence Started

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const Agence = () => {
   return (
     <>
-      <div className="">
-        <div>
-          
+      <div >
+        <div className="mt-[55vh]">
+          <h1 className="h-screen font-[Quintessa500] text-[20vw] leading-[16.5vw] uppercase text-center ">
+            Soixan7e <br /> Douze
+          </h1>
         </div>
+
       </div>
     </>
   );
